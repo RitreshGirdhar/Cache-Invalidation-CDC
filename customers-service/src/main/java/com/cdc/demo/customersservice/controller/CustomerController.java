9ce6a38ff2afb730e50843611cd71313f9d9ceba
Ritresh Girdhar <ritgirdh@publicisgroupe.net>
2021-01-27 21:26:45 +0530

    docker compose file addition

--- customers-service/src/main/java/com/cdc/demo/customersservice/controller/CustomerController.java
+++ customers-service/src/main/java/com/cdc/demo/customersservice/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.cdc.demo.customersservice.models.CustomerResponse;
 import com.cdc.demo.customersservice.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
